bd2dd84af5899d0d1e6564e380ec426c011808fb
Brannon Labra <brannonalabra@gmail.com>
2020-10-22 05:29:17 +0000

    added if statements for pay economical and pay gourmet, added if statements for loadMoney, added void method loadMoney, added billy and brian scenario

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,34 @@
 public class TerrapinCard {
+  private double balance;
 
+  public TerrapinCard(double balance){
+    this.balance = balance;
+  }
 
+  public String getBalance(){
+    return "The card has " + balance + " dollars";
+  }
+
+  public void payEconomical(){
+    if (balance >= 2.50){
+      balance -= 2.50;
+    }
+  }
+
+  public void payGourmet(){
+    if (balance >= 4.00){
+     balance -= 4.00;
+    }
+  }
+
+  public void loadMoney(double amount){
+    if (amount > 0.0){
+     balance += amount;
+   }
+
+   if (balance > 150.0){
+     balance = 150.0;
+   }
+  
+  }
 }
